154584bd8ccb95fd4848d0dac659fb86c2f4df25
Mario Ernesto Castillo <marioe.castillo93@gmail.com>
2017-03-09 17:16:33 -0500

    loginFacebook
    Para este codigo ya se pueden ver muchos de los campos del perfil del
    usuario. Falta pasar esos datos a otra actividad.

--- app/src/main/java/com/example/mario/loginfacebook/MainActivity.java
+++ app/src/main/java/com/example/mario/loginfacebook/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mario.loginfacebook;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.facebook.CallbackManager;
@@ -17,15 +20,22 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mensaje;
     private LoginButton loginButton;
+    private ProfilePictureView imageProfile;
+
     private CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         loginButton= (LoginButton) findViewById(R.id.login_button);
         mensaje= (TextView) findViewById(R.id.mensajedelogin);
+        imageProfile= (ProfilePictureView) findViewById(R.id.imageprofile);
 
         loginButton.setReadPermissions(Arrays.asList(
                 "public_profile", "email", "user_birthday", "user_friends"));
@@ -51,7 +62,12 @@ public class MainActivity extends AppCompatActivity {
                                     GraphResponse response) {
                                 try {
                                     mensaje.setText(object.getString("name"));
+                                    imageProfile.setProfileId(object.getString("id"));
                                     Log.d("link",object.getString("link"));
+                                    Log.d("link",object.getString("email"));
+                                    Log.d("link",object.getString("birthday"));
+                                    Log.d("link",object.getString("gender"));
+                                    Log.d("link", object.getJSONObject("picture").getJSONObject("data").getString("url"));
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
@@ -59,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                         });
                 Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,name,link");
+                parameters.putString("fields", "id,name,link,email,birthday,picture.type(normal),gender");
                 request.setParameters(parameters);
                 request.executeAsync();
 
